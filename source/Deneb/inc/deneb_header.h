ee720d7657723ef00de0bfa11f7b1f1c709f599a
Hojun You <hojun.you.kr@gmail.com>
2022-09-19 17:52:50 +0900

    Deneb Build Version update (v1.0.1)

--- source/Deneb/inc/deneb_header.h
+++ source/Deneb/inc/deneb_header.h
@@ -2,7 +2,7 @@
 
 #define DENEB_MAJOR_VERSION 1
 #define DENEB_MINOR_VERSION 0
-#define DENEB_BUILD_VERSION 0
+#define DENEB_BUILD_VERSION 1
 #define DENEB_VERSION                                     \
   "Deneb v" + std::to_string(DENEB_MAJOR_VERSION) + "." + \
       std::to_string(DENEB_MINOR_VERSION) + "." +         \
